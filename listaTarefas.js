58fa14d042e99c18c89d261fa6320f9c28ee95f0
bernaRocha <mrochabernardo@gmail.com>
2021-09-28 11:59:54 -0300

    adição da tarefa na página

--- listaTarefas.js
+++ listaTarefas.js
@@ -1,7 +1,28 @@
-const novaTarefa = document.querySelector('[data-form-button]')
+const criarTarefa = (evento) => {
+  
+  evento.preventDefault()
 
-novaTarefa.addEventListener('click', () => {
+  const lista = document.querySelector('[data-list]')
   const input = document.querySelector('[data-form-input]')
   const valor = input.value
-  console.log(valor)
-})
+  
+  const tarefa = document.createElement('li') // aqui o elemento li é criado
+  tarefa.classList.add('task')  // a tarefa ganha a class do css de nome task
+
+  const conteudo = `<p class='content'>${valor}</p>`
+
+  tarefa.innerHTML = conteudo  // na li é adicionado o conteudo da variável da linha 12
+
+  lista.appendChild(tarefa)  // cria um elemento filho da li que é a tarefa
+  input.value = ' ' // após adicionar a tarefa o campo do input é zerado
+
+}
+
+const novaTarefa = document.querySelector('[data-form-button]') // novaTarefa pega o seletor data-form-button
+
+novaTarefa.addEventListener('click', criarTarefa) // novaTarefa escuta o evento de click e ao escutar executa o criarTarefa
+
+const botaoConclui = () => {
+    const botaoConclui = document.createElement('button')
+
+}
