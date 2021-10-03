f4e3cefdade7bcd7c400c7dbd110e5ba3a671f8f
bernaRocha <mrochabernardo@gmail.com>
2021-10-03 14:48:28 -0300

    comentários no código

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-import BotaoConclui from "./components/concluiTarefa.js"
-import BotaoDeleta from "./components/deletaTarefa.js"
+import BotaoConclui from "./components/concluiTarefa.js" //Importação do módulo concluiTarefa
+import BotaoDeleta from "./components/deletaTarefa.js"   
 
   const criarTarefa = (evento) => {
   
   evento.preventDefault()
 
-  const lista = document.querySelector('[data-list]')
-  const input = document.querySelector('[data-form-input]')
-  const valor = input.value
+  const lista = document.querySelector('[data-list]') // Linha 23 do HTML
+  const input = document.querySelector('[data-form-input]') // Linha 19, pega o Input
+  const valor = input.value // const valor é o value do Input
   
   const tarefa = document.createElement('li') // aqui o elemento li é criado
-  tarefa.classList.add('task')  // a tarefa ganha a class do css de nome task
+  tarefa.classList.add('task')  // a tarefa ganha a class do css de nome task, linha 118 do arquivo css
 
-  const conteudo = `<p class='content'>${valor}</p>`
+  const conteudo = `<p class='content'>${valor}</p>` // O conteudo é um parágrafo com o valor, linha 10 deste arquivo
 
   tarefa.innerHTML = conteudo  // na li é adicionado o conteudo da variável da linha 12
 
-  tarefa.appendChild(BotaoConclui()) // adiciona a função botaoConclui
-  tarefa.appendChild(BotaoDeleta())
+  tarefa.appendChild(BotaoConclui()) // adiciona a função BotaoConclui()
+  tarefa.appendChild(BotaoDeleta()) // adiciona a função BotaoDeleta()
   lista.appendChild(tarefa)  // cria um elemento filho da li que é a tarefa
   input.value = ' ' // após adicionar a tarefa o campo do input é zerado
 
